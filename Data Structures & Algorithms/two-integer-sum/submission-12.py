d3b46e1ea1363fefcc72b1da8410a776ef4ce1c5
Jms.manion <Jms.manion@gmail.com>
2026-07-08 14:35:07 -0400

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap = {}
+
+        for i, n in enumerate(nums):
+            if n in hmap:
+                return [hmap[n], i]
+            hmap[target - n] = i
+        return []
